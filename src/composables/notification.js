a01188aa84be5cc790d00fc058d87be0d28d62b2
Riccardo De Lucia <riccardo.delucia@fht.org>
2022-02-01 15:09:19 +0100

    notification on login

--- src/composables/notification.js
+++ src/composables/notification.js
@@ -1,6 +1,7 @@
 import store from "@/store/index.js";
 
 export const sendNotification = ({ type = "success", message = "" }) => {
+  console.log("send notification");
   const notification = { type, message };
   store.dispatch("notification/add", notification);
 };
