4b197162793b92ad9af2d53db41a1b862dc9b6bd
Riccardo De Lucia <riccardo.delucia@fht.org>
2022-02-08 18:19:50 +0100

    new results page arrangement

--- src/views/ccr/ViewCRISPRcleanRResultsByID.vue
+++ src/views/ccr/ViewCRISPRcleanRResultsByID.vue
@@ -1,26 +1,82 @@
 <template>
   <BaseLayoutApp>
     <div class="layout-ccr">
-      <h2 class="u-margin-bottom-medium">Results</h2>
-      <div>
-        <h3 class="u-margin-bottom-small">Details</h3>
-        <ul>
-          <li><b>Title: </b>{{ result.title }}</li>
-          <li><b>Date: </b>{{ result.dateTime }}</li>
-          <li><b>Status: </b>{{ result.status }}</li>
-          <li><b>Input counts file: </b>{{ result.fileCountsName }}</li>
-          <li><b>Library: </b>{{ result.library }}</li>
-          <li><b>Number of controls: </b>{{ result.nControls }}</li>
-          <li>
-            <b>Minimal number of reads in the control sample: </b
-            >{{ result.normMinReads }}
-          </li>
-          <li><b>Normalization Method: </b>{{ result.method }}</li>
-          <li v-if="result.notes"><b>Notes: </b>{{ result.notes }}</li>
-        </ul>
+      <h2 class="u-margin-bottom-small">Results</h2>
+      <div class="ccr-results">
+        <div
+          v-if="result.status === 'success'"
+          class="card ccr-results__downloads"
+        >
+          <h3 class="u-margin-bottom-small">Downloads</h3>
+          <div>
+            <button
+              v-for="(file, index) in fileList"
+              :key="index"
+              @click="onClick(file, id)"
+              class="button button--primary button--small"
+              type="button"
+            >
+              {{ file }}&nbsp;<span><BaseIcon name="download" /></span>
+            </button>
+          </div>
+        </div>
+        <div class="card ccr-results__genes-signatures">
+          <h3 class="u-margin-bottom-small">Genes Signatures</h3>
+          <GeneSignatures :data="genesSignatures"> </GeneSignatures>
+        </div>
+        <div class="card ccr-results__details">
+          <h3 class="u-margin-bottom-small">Details</h3>
+          <ul>
+            <li><b>Title: </b>{{ result.title }}</li>
+            <li><b>Date: </b>{{ result.dateTime }}</li>
+            <li><b>Status: </b>{{ result.status }}</li>
+            <li><b>Input counts file: </b>{{ result.fileCountsName }}</li>
+            <li><b>Library: </b>{{ result.library }}</li>
+            <li><b>Number of controls: </b>{{ result.nControls }}</li>
+            <li>
+              <b>Minimal number of reads in the control sample: </b
+              >{{ result.normMinReads }}
+            </li>
+            <li><b>Normalization Method: </b>{{ result.method }}</li>
+            <li v-if="result.notes"><b>Notes: </b>{{ result.notes }}</li>
+          </ul>
+        </div>
+        <div
+          class="card ccr-results__thumbnails ccr-results__thumbnails--color1"
+        >
+          <h3 class="u-margin-bottom-small">Main Charts</h3>
+          <div>
+            <BaseThumbnail
+              v-for="item in normImages"
+              :key="item.filename"
+              @click="openModal(item, id)"
+              :img="item"
+            ></BaseThumbnail>
+            <BaseThumbnail
+              v-for="item in qcImages"
+              :key="item.filename"
+              @click="openModal(item, id)"
+              :img="item"
+            ></BaseThumbnail>
+          </div>
+        </div>
+        <div
+          class="card ccr-results__thumbnails ccr-results__thumbnails--color2"
+        >
+          <h3 class="u-margin-bottom-small">Chromosome Charts</h3>
+          <div>
+            <BaseThumbnail
+              v-for="item in chrImages"
+              :key="item.filename"
+              @click="openModal(item, id)"
+              :img="item"
+            ></BaseThumbnail>
+          </div>
+        </div>
       </div>
+
       <template v-if="result.status === 'success'">
-        <div class="downloads-container u-margin-top-small">
+        <!-- <div class="downloads-container u-margin-top-small">
           <button
             v-for="(file, index) in fileList"
             :key="index"
@@ -30,38 +86,29 @@
           >
             {{ file }}&nbsp;<span><BaseIcon name="download" /></span>
           </button>
-        </div>
-        <h3 class="u-margin-bottom-small">Genes Signatures</h3>
-        <GeneSignatures :data="genesSignatures"> </GeneSignatures>
-        <h3 class="u-margin-bottom-small">Normalization</h3>
-        <div class="results-container">
-          <BaseThumbnail
-            v-for="item in normImages"
-            :key="item.filename"
-            @click="openModal(item, id)"
-            :img="item"
-          ></BaseThumbnail>
-        </div>
+        </div> -->
+        <!--         <h3 class="u-margin-bottom-small">Genes Signatures</h3>
+        <GeneSignatures :data="genesSignatures"> </GeneSignatures> -->
 
-        <h3 class="u-margin-bottom-small">Cleaned chromosomes</h3>
-        <div class="results-container">
+        <!--         <h3 class="u-margin-bottom-small">Cleaned chromosomes</h3>
+        <div class="ccr-results__thumbnails">
           <BaseThumbnail
             v-for="item in chrImages"
             :key="item.filename"
             @click="openModal(item, id)"
             :img="item"
           ></BaseThumbnail>
-        </div>
+        </div> -->
 
-        <h3 class="u-margin-bottom-small">QC assessment</h3>
-        <div class="results-container">
+        <!--         <h3 class="u-margin-bottom-small">QC assessment</h3>
+        <div class="ccr-results__thumbnails">
           <BaseThumbnail
             v-for="item in qcImages"
             :key="item.filename"
             @click="openModal(item, id)"
             :img="item"
           ></BaseThumbnail>
-        </div>
+        </div> -->
         <BaseModal v-if="modalState != 'closed'" @modal-close="closeModal">
           <template v-slot:header>{{ image.label }} </template>
           <template v-slot:body>
@@ -74,9 +121,9 @@
           </template>
         </BaseModal>
       </template>
-      <p v-else>
+      <!--  <p v-else>
         Further content will appear here upon successful job completion...
-      </p>
+      </p> -->
     </div>
   </BaseLayoutApp>
 </template>
@@ -186,17 +233,67 @@ export default {
 </script>
 
 <style lang="scss">
-.downloads-container {
+/* .downloads-container {
   margin-bottom: 1em;
   display: flex;
   gap: 1em;
   flex-wrap: wrap;
 }
-
-.results-container {
+ */
+/* .results-container {
   display: grid;
   grid-gap: 2rem;
   grid-template-columns: repeat(auto-fit, 32rem);
   margin-bottom: 2em;
+} */
+
+.ccr-results {
+  display: grid;
+  grid-template-columns: repeat(8, minmax(min-content, 14rem)) 1fr;
+  grid-column-gap: 1.2em;
+  grid-row-gap: 1.5em;
+  margin-bottom: 2em;
+  //grid-template-rows: repeat()
+
+  &__details {
+    //justify-self: left;
+    grid-column: 4 / 8;
+    grid-row: 1 / 2;
+  }
+
+  &__downloads {
+    grid-column: 4 / 8;
+    grid-row: 2 / 3;
+
+    div {
+      margin-bottom: 1em;
+      display: flex;
+      gap: 1em;
+      flex-wrap: wrap;
+    }
+  }
+  &__genes-signatures {
+    grid-column: 1 / 4;
+    grid-row: 1 / 3;
+  }
+
+  &__thumbnails {
+    grid-column: 1/ -1;
+    //width: min-content;
+    //justify-self: left;
+
+    &--color1 {
+      background-color: rgb(232, 232, 253);
+    }
+    &--color2 {
+      background-color: rgb(212, 255, 208);
+    }
+
+    div {
+      display: grid;
+      grid-gap: 2rem;
+      grid-template-columns: repeat(auto-fit, 32rem);
+    }
+  }
 }
 </style>
