2de5ffebd87445b9fff7fe941472fc23cd1f1c93
Riccardo Roberto De Lucia <riccardo.delucia@fht.org>
2022-04-22 13:38:52 +0200

    Update .gitlab-ci.yml, deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -2,6 +2,7 @@
 
 source /home/riccardo.delucia/docker-compose/bin/activate
 cd /home/riccardo.delucia/deploy/iorio-apps
+docker login -u riccardo.delucia -p $DEPLOY_TOKEN_COMBIO_DEV01
 docker-compose pull
 docker-compose down
 docker-compose -p iorio_apps up --no-build -d
