189eb9a3d5d32a311ad72ba62c23fa7906aaaa2a
riccardo.delucia <riccardo.delucia@fht.org>
2022-04-22 14:03:00 +0200

    aaa

--- deploy.sh
+++ deploy.sh
@@ -3,7 +3,7 @@
 source /home/riccardo.delucia/docker-compose/bin/activate
 cd /home/riccardo.delucia/deploy/iorio-apps
 
-docker login -u riccardo.delucia -p 5B6DrTsrp9JBVqYDStPF gitlab.fht.org:5050/riccardo.delucia/iorio-apps/iorio_apps
+#docker login -u riccardo.delucia -p 5B6DrTsrp9JBVqYDStPF gitlab.fht.org:5050/riccardo.delucia/iorio-apps/iorio_apps
 docker-compose pull
 docker-compose down
-docker-compose -p iorio_apps up -d
+docker-compose -p iorio_apps up --no-build -d
