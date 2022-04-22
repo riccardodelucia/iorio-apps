4151c3067c59fbf418e9243d871bb7d51ef2ae7c
riccardo.delucia <riccardo.delucia@fht.org>
2022-04-22 14:22:44 +0200

    1

--- deploy.sh
+++ deploy.sh
@@ -2,6 +2,6 @@
 
 source /home/riccardo.delucia/docker-compose/bin/activate
 cd /home/riccardo.delucia/deploy/iorio-apps
-docker-compose pull
-docker-compose down
-docker-compose -p iorio_apps up --no-build -d
+#docker-compose pull
+#docker-compose down
+#docker-compose -p iorio_apps up --no-build -d
